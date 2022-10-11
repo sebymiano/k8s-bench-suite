85e31e316958cb051eb991f4c47c518906fa3135
Sebastiano Miano <mianosebastiano@gmail.com>
2022-10-11 16:47:47 +0100

    Update scripts

--- cni-tests/polykube/start-polykube-k8s.sh
+++ cni-tests/polykube/start-polykube-k8s.sh
@@ -59,12 +59,13 @@ kubectl apply -f manifests
 popd
 
 echo -e "${COLOR_GREEN}[ INFO ] Polykube CNI started, wait until all service boot up.${COLOR_OFF}"
-secs=$((90))
-while [ $secs -gt 0 ]; do
-   echo -ne "Countdown: $secs\033[0K\r"
-   sleep 1
-   : $((secs--))
-done
+kubectl wait --for=condition=Ready nodes --all --timeout=90s
+# secs=$((90))
+# while [ $secs -gt 0 ]; do
+#    echo -ne "Countdown: $secs\033[0K\r"
+#    sleep 1
+#    : $((secs--))
+# done
 
 echo -e "${COLOR_GREEN}[ INFO ] Restart all containers ${COLOR_OFF}"
 kubectl get pods --all-namespaces -o custom-columns=NAMESPACE:.metadata.namespace,NAME:.metadata.name,HOSTNETWORK:.spec.hostNetwork --no-headers=true | grep '<none>' | awk '{print "-n "$1" "$2}' | xargs -L 1 -r kubectl delete pod --grace-period=15
