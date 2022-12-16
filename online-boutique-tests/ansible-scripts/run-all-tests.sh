fc370e5ad1af4286bd1196959ef60afa9af83090
Sebastiano Miano <mianosebastiano@gmail.com>
2022-12-16 13:41:03 +0100

    Update ansible scripts

--- online-boutique-tests/ansible-scripts/run-all-tests.sh
+++ online-boutique-tests/ansible-scripts/run-all-tests.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 ansible-playbook -i hosts cilium-test-25k.yaml
+sleep 10
+
 ansible-playbook -i hosts calico-test-25k.yaml
+sleep 10
+
 ansible-playbook -i hosts polykube-test-25k.yaml
+sleep 10
+
 ansible-playbook -i hosts morpheus-full-test-25k.yaml
+sleep 10
+
 ansible-playbook -i hosts morpheus-only-router-test-25k.yaml
+sleep 10
