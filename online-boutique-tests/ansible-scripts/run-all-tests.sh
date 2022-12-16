7827721deadaf1c7c81941c024e11fd86fc792a7
Sebastiano Miano <mianosebastiano@gmail.com>
2022-12-16 11:10:11 +0000

    Add ansible scripts to run tests automatically

---
+++ online-boutique-tests/ansible-scripts/run-all-tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ansible-playbook -i hosts cilium-test-25k.yaml
+ansible-playbook -i hosts calico-test-25k.yaml
+ansible-playbook -i hosts polykube-test-25k.yaml
+ansible-playbook -i hosts morpheus-full-test-25k.yaml
+ansible-playbook -i hosts morpheus-only-router-test-25k.yaml
